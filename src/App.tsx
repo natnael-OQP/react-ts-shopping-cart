64eeac5dcf9c854dfcdec46090398542b6121dbe
natnael-OQP <makiya112545@gmail.com>
2022-08-24 22:48:17 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
