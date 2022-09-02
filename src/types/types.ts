1e877b7fe021a1c6d27b8f533ed0503c600a6a59
natnael-OQP <makiya112545@gmail.com>
2022-09-02 23:01:38 +0300

    define types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,24 @@
+import { ReactNode } from "react";
+
 export type StoreItemProps = {
     id: number;
     name: string;
     price: number;
     imgUrl: string;
 };
+
+export type ShoppingCartProviderProps={
+    children:ReactNode;
+}
+
+export type ShoppingCartContextProps={
+    getItemQuantity:(id:number) => number;
+    incrementCartQuantity:(id:number)=>void;
+    decrementCartQuantity:(id:number)=>void;
+    removeFromCart:(id:number)=>void;
+}
+
+export type CartItemProps={
+    id:number;
+    quantity:number;
+}
